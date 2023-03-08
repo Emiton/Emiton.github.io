c40540bf78959324b075048ed53d84b1cd05e540
Emiton Alves <emitonalves@gmail.com>
2023-03-08 16:11:24 -0500

    Updated to https

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <h1>🚧</h1>
       <div className="gif">
-        <img src="images/construction-doggy.gif"></img>
+        <img src="/images/construction-doggy.gif" alt="Construction doggo"></img>
       </div>
       <h1>🚧</h1>
       <h1>Under Construction</h1>
-      <a href="pdf/EmitonAlvesResume.pdf" target="_blank"><h2>Resume</h2></a>
+      <a href="/pdf/EmitonAlvesResume.pdf" target="_blank"><h2>Resume</h2></a>
     </div>
   );
 }
