5c823797153a23bdf3398df6232c17d8478b140f
Emiton Alves <emitonalves@gmail.com>
2023-03-10 00:27:44 -0500

    Added ES lint and prettier to project

--- personal-website/src/data.js
+++ personal-website/src/data.js
@@ -1,5 +1,36 @@
-export const TOOLS = {
-  'JavaScript': '<i class="devicon-javascript-plain"></i>',
-  'HTML': '<i class="devicon-html5-plain-wordmark"></i>',
-  'CSS': '<i class="devicon-css3-plain-wordmark"></i>'
-}
+export const TOOLS = [
+  { tool: 'JavaScript', icon: '<i class="devicon-javascript-plain"></i>' },
+  { tool: 'HTML', icon: '<i class="devicon-html5-plain-wordmark"></i>' },
+  { tool: 'CSS', icon: '<i class="devicon-css3-plain-wordmark"></i>' },
+];
+
+export const COMPANY_INFO = [
+  {
+    companyName: 'Amazon Go',
+    jobTitle: 'Full-stack Software Engineer',
+    workLocation: 'Westborough, MA',
+    workDates: 'June 2021 - Present',
+    workDescription: 'Full-stack engineer working on stuff like this that and the third. Full-stack engineer working on stuff like this that and the third. Full-stack engineer working on stuff like this that and the third',
+  },
+  {
+    companyName: 'Amazon Robotics',
+    jobTitle: 'Full-stack Software Engineer',
+    workLocation: 'Westborough, MA',
+    workDates: 'June 2021 - Present',
+    workDescription: 'Full-stack engineer working on stuff like this that and the third. Full-stack engineer working on stuff like this that and the third. Full-stack engineer working on stuff like this that and the third',
+  },
+  {
+    companyName: 'Massachusetts Disabled Persons Protection Commission',
+    jobTitle: 'Full-stack Software Engineer',
+    workLocation: 'Westborough, MA',
+    workDates: 'June 2021 - Present',
+    workDescription: 'Full-stack engineer working on stuff like this that and the third. Full-stack engineer working on stuff like this that and the third. Full-stack engineer working on stuff like this that and the third',
+  },
+];
+
+/*
+  'Twitter': {},
+  'University of Rhode Island': {}
+  'FM Global': {},
+  'Continental Tires': {}
+*/
