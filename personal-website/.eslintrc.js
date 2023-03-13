267e8682575e56e5c6f5bfd3c1146fd440dde9ba
Emiton Alves <emitonalves@gmail.com>
2023-03-13 19:32:49 -0400

    Updated lint rules to not worry about importing whole packages

--- personal-website/.eslintrc.js
+++ personal-website/.eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }],
+    'import/no-extraneous-dependencies': ["error", {"devDependencies": false, "optionalDependencies": false, "peerDependencies": false}],
   },
 };
